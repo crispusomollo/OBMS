753bb84eb88d3b5099129b57cad1e16be73cf2c1
crispusomollo <crispusomollo@gmail.com>
2024-11-20 13:34:56 +0300

    updated the main form form1

--- Form1.cs
+++ Form1.cs
@@ -1,9 +1,36 @@
-namespace OBMS;
+using System;
+using System.Windows.Forms;
 
-public partial class Form1 : Form
+namespace OBMS
 {
-    public Form1()
+    public partial class Form1 : Form
     {
-        InitializeComponent();
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Set Form Properties
+            this.Text = "Bus Management System";
+            this.Size = new System.Drawing.Size(500, 300);
+
+            // Welcome Label
+            Label lblWelcome = new Label();
+            lblWelcome.Text = "Welcome to the Bus Management System!";
+            lblWelcome.AutoSize = true;
+            lblWelcome.Location = new System.Drawing.Point(20, 20);
+            this.Controls.Add(lblWelcome);
+
+            // Login Button
+            Button btnLogin = new Button();
+            btnLogin.Text = "Login";
+            btnLogin.Location = new System.Drawing.Point(20, 60);
+            btnLogin.Click += BtnLogin_Click;
+            this.Controls.Add(btnLogin);
+        }
+
+        private void BtnLogin_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Login functionality is not implemented yet!");
+        }
     }
 }
